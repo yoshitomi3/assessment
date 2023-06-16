88447ff32ef8e0914a812840c11dd09bb5c154d1
yoshitomi3 <136287870+yoshitomi3@users.noreply.github.com>
2023-06-16 13:29:55 +0900

    診断結果を変更

--- assessment.js
+++ assessment.js
@@ -12,14 +12,29 @@ assessmentButton.onclick = () => {
   
   // 診断結果表示エリアの作成
   resultDivision.innerText = '';
-  const header = document.createElement('h3');
-  header.innerText = '診断結果';
-  resultDivision.appendChild(header);
+  
+  // headerDivision の作成
+  const headerDivision = document.createElement('div');
+  headerDivision.setAttribute('class', 'card-header');
+  headerDivision.innerText = '診断結果';
+
+  // bodyDivision の作成
+  const bodyDivision = document.createElement('div');
+  bodyDivision.setAttribute('class', 'card-body');
 
   const paragraph = document.createElement('p');
+  paragraph.setAttribute('class', 'card-text');
   const result = assessment(userName);
   paragraph.innerText = result;
-  resultDivision.appendChild(paragraph);
+  bodyDivision.appendChild(paragraph);
+
+  // resultDivision に Bootstrap のスタイルを適用する
+  resultDivision.setAttribute('class', 'card');
+  resultDivision.setAttribute('style', 'max-width: 700px;')
+
+  // headerDivision と bodyDivision を resultDivision に差し込む
+  resultDivision.appendChild(headerDivision);
+  resultDivision.appendChild(bodyDivision);
 
   // ツイートエリアの作成
   tweetDivision.innerText = '';
